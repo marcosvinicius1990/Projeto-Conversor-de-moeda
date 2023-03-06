c9ee6be478e05ca241d124a2cd23589e4758732a
MarcosVinicius1990 <pereiradasilva@yahoo.com.br>
2023-03-05 21:06:49 -0300

    Conversor de moedas

---
+++ assets/conversor.js
@@ -0,0 +1,61 @@
+const button = document.getElementById("convert-button")
+const select = document.getElementById("currency-select")
+
+const dolar = 5.2
+const euro = 5.9
+
+
+const convertValues = () => {
+    const inputReais = document.getElementById("input-real").value
+    const realValueText = document.getElementById("real-value-text")
+    const currencyValueText = document.getElementById("currency-value-text")
+
+    realValueText.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+    }).format(inputReais)
+
+
+    if (select.value === "U$ Dólar Americano")   {
+        currencyValueText.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD",
+        }).format(inputReais / dolar)
+    }
+
+
+    if (select.value === "€ Euro") {
+        currencyValueText.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR",
+        }).format(inputReais / euro)
+    }
+
+   
+
+}
+
+changeCurrency = () => {
+    const currencyName = document.getElementById("currency-name")
+    const currencyImg = document.getElementById("currency-img")
+
+    if (select.value === "U$ Dólar Americano") {
+        currencyName.innerHTML = "Dólar americano"
+        currencyImg.src = "./bandeiraamericana.png"
+    }
+
+    if (select.value === "€ Euro") {
+        currencyName.innerHTML = "Euro"
+        currencyImg.src = "./simbolo-euro.webp"
+    }
+
+   
+
+    convertValues()
+
+}
+
+
+
+button.addEventListener("click", convertValues)
+select.addEventListener("change", changeCurrency)
